2b5351565470dd3ce5c8543584ecf92c330c283f
KSJ0128 <ksj01128@naver.com>
2024-09-22 17:07:31 +0900

    Initialize

---
+++ BEF/src/main/java/com/example/BEF/BefApplication.java
@@ -0,0 +1,13 @@
+package com.example.BEF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BefApplication.class, args);
+	}
+
+}
---
+++ BEF/src/test/java/com/example/BEF/BefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BEF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
