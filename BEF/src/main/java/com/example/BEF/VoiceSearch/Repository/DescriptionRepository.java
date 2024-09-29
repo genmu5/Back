616ab17e996c331091ed21ba80a44f2df8e4f911
genmu5 <genmu4@skuniv.ac.kr>
2024-09-29 22:52:21 +0900

    주석 추가

--- BEF/src/main/java/com/example/BEF/VoiceSearch/Repository/DescriptionRepository.java
+++ BEF/src/main/java/com/example/BEF/VoiceSearch/Repository/DescriptionRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface DescriptionRepository extends JpaRepository<Location, Long> {
-
+    // 개요 컬럼에서 키워드가 존재하면 해당 개요 컬럼이 있는 튜플 전부 반환
     @Query("SELECT l FROM Location l WHERE l.description LIKE %:keyword%")
     List<Location>findDescription(@Param("keyword") String keyword);
 }
--- BEF/src/main/java/com/example/BEF/VoiceSearch/Service/OpenAIClientService.java
+++ BEF/src/main/java/com/example/BEF/VoiceSearch/Service/OpenAIClientService.java
@@ -30,6 +30,7 @@ public class OpenAIClientService {
         return openAIClient.createTranscription(whisperTranscriptionRequest);
     }
 
+    // Keyword의 해당하는 단어 탐색하여 음성 텍스트 필터링 Keywords 배열에서 앞에 있을 수록 단어 우선순위가 높다.
     public String filterTranscription(String transcribedText) {
         List<String> Keywords = Arrays.asList("공원","바다","서울");
 
@@ -38,7 +39,7 @@ public class OpenAIClientService {
                 .findFirst()
                 .orElse("키워드 없음");
     }
-
+    // location 테이블에서 키워드에 해당하는 튜플 조회후 리스트 형식으로 반환
     public List<Location> searchLocationsByKeyword(String transcribedText) {
         String keyword = filterTranscription(transcribedText);
 
