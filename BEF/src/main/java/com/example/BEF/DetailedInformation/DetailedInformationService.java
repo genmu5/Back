1152f6a9467b67fe6d2c1776db72ff224ced6bc8
genmu5 <genmu4@skuniv.ac.kr>
2024-10-04 17:58:28 +0900

    모든 Controller Cors 접근 허용, 관광지 상세정보 조회 기능

---
+++ BEF/src/main/java/com/example/BEF/DetailedInformation/DetailedInformationService.java
@@ -0,0 +1,27 @@
+package com.example.BEF.DetailedInformation;
+
+import com.example.BEF.Disabled.Domain.Disabled;
+import com.example.BEF.Disabled.Service.DisabledRepository;
+import com.example.BEF.Location.Domain.Location;
+import com.example.BEF.Location.Service.LocationRepository;
+import com.example.BEF.MapLocation.DTO.MapLocationResponse;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class DetailedInformationService {
+    private final LocationRepository locationRepository;
+    private final DisabledRepository disabledRepository;
+
+    public MapLocationResponse getLocationDetailed(Long contetntId){
+        Location location = locationRepository.findLocationByContentId(contetntId);
+        Disabled disabled = disabledRepository.findDisabledByLocation(location);
+
+        return new MapLocationResponse(location, disabled);
+    }
+
+}
---
+++ BEF/src/main/java/com/example/BEF/WebConfig/WebConfig.java
@@ -0,0 +1,19 @@
+package com.example.BEF.WebConfig;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")  // 모든 엔드포인트에 대해 CORS 허용
+                .allowedOrigins("http://localhost:5137")  // 허용할 출처 (도메인)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")  // 허용할 HTTP 메서드
+                .allowedHeaders("*")  // 허용할 헤더
+                .allowCredentials(true)  // 인증정보 허용
+                .maxAge(3600);  // CORS 응답 캐시 시간 (초)
+    }
+}
