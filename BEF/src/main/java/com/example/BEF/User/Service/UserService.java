a8e985dbba4cdab5a1cf61f6df8e17e061e7a4da
KSJ0128 <ksj01128@naver.com>
2025-01-28 20:00:17 +0900

    refact: 유저 생성 로직 수정
    유저 도메인 변경 및 장애 유형, 여행 타입 수정에 따른 유저 생성 로직을 변경했습니다.

--- BEF/src/main/java/com/example/BEF/User/Service/UserService.java
+++ BEF/src/main/java/com/example/BEF/User/Service/UserService.java
@@ -2,6 +2,12 @@ package com.example.BEF.User.Service;
 
 import com.example.BEF.Course.Domain.Course;
 import com.example.BEF.Course.Repository.CourseRepository;
+import com.example.BEF.Disability.DisabilityRepository;
+import com.example.BEF.Disability.UserDisability;
+import com.example.BEF.Disability.UserDisabilityRepository;
+import com.example.BEF.TripType.TripTypeRepository;
+import com.example.BEF.TripType.UserTripType;
+import com.example.BEF.TripType.UserTripTypeRepository;
 import com.example.BEF.User.DTO.UserDisabledDTO;
 import com.example.BEF.User.DTO.UserJoinReq;
 import com.example.BEF.User.DTO.UserJoinRes;
@@ -14,42 +20,42 @@ import org.springframework.stereotype.Service;
 public class UserService {
     private final UserRepository userRepository;
     private final CourseRepository courseRepository;
+    private final TripTypeRepository tripTypeRepository;
+    private final UserTripTypeRepository userTripTypeRepository;
+    private final DisabilityRepository disabilityRepository;
+    private final UserDisabilityRepository userDisabilityRepository;
 
     public UserJoinRes saveUser(UserJoinReq userJoinReq) {
-        // 저장할 유저 생성
-        User savedUser = new User();
-
         // 회원가입 가능 여부 확인
         if (!userJoinReq.validJoin())
             return (null);
 
-        // 유저 정보 기입
-        savedUser.setUserName(userJoinReq.getUserName());
-        savedUser.setGender(userJoinReq.getGender());
-        savedUser.setAge(userJoinReq.getAge());
-        savedUser.setSenior(userJoinReq.getSenior());
-        savedUser.setWheelchair(userJoinReq.getWheelchair());
-        savedUser.setBlindHandicap(userJoinReq.getBlindHandicap());
-        savedUser.setHearingHandicap(userJoinReq.getHearingHandicap());
-        savedUser.setInfantsFamily(userJoinReq.getInfantsFamily());
-        savedUser.setUuid(userJoinReq.getUuid()); // uuid
-
-        // 유저 여행 타입 설정
-        savedUser.setForest(userJoinReq.getTravelType().contains("forest"));
-        savedUser.setOcean(userJoinReq.getTravelType().contains("ocean"));
-        savedUser.setCulture(userJoinReq.getTravelType().contains("culture"));
-        savedUser.setOutside(userJoinReq.getTravelType().contains("outside"));
+        // 저장할 유저 생성
+        User savedUser = new User(userJoinReq.getUserName(),
+                userJoinReq.getGender(),
+                userJoinReq.getBirth());
 
         // 유저 저장
         userRepository.save(savedUser);
+
+        // 유저 여행 타입 저장
+        userTripTypeRepository.saveAll(userJoinReq.getTripType().stream()
+                .map(tripTypeRepository::findByName)
+                .map(tripTypeEntity -> UserTripType.of(savedUser, tripTypeEntity))
+                .toList());
+
+        // 유저 장애 유형 저장
+        userDisabilityRepository.saveAll(userJoinReq.getDisability().stream()
+                .map(disabilityRepository::findByName)
+                .map(disabilityEntity -> UserDisability.of(savedUser, disabilityEntity))
+                .toList());
 
         // 유저 저장한 관광지 리스트
         Course saveCourse = new Course(savedUser, "저장", "");
         courseRepository.save(saveCourse);
 
         // 유저 정보 리턴
-//        return (new UserJoinRes(savedUser.getUserNumber(), savedUser.getUserName())); // uuid
-        return (new UserJoinRes(savedUser.getUserName(), savedUser.getUuid())); // uuid
+        return (new UserJoinRes(savedUser.getUserNumber(), savedUser.getUserName()));
     }
 
 //    public UserDisabledDTO settingUserDisabled(Long userNumber) {
